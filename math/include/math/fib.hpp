52bc78535985015063944459ac21fe8990f8e3fe
DartSnack <leo2004ovs1@gmail.com>
2024-12-25 00:32:20 +0300

    Fix docs comments

--- math/include/math/fib.hpp
+++ math/include/math/fib.hpp
@@ -8,16 +8,16 @@ namespace math
     inline constexpr std::int32_t badfib = -1;
 
     /**
-     * @brief Calculates the Fibonacci sequence (n-1)`th number.
+     * @brief Calculates the Fibonacci sequence (n+1)`th number.
      *
      * Uses recursion for its calculation, optimized by caching an already
      * calculated value.
      * It is reasonable to support only first 45 numbers, since higher values
      * might cause `int32_t` arithmetic overflow.
      *
-     * @param n Index of the Fibonacci sequence number. Must be less than 46.
+     * @param n Index of the Fibonacci sequence number. Must be less than 47.
      * 
-     * @return The (n-1)'th Fibonacci sequence number.
+     * @return The (n+1)'th Fibonacci sequence number.
      * @return `badfib` on failure.
      */
     std::int32_t fib(std::int32_t n);
